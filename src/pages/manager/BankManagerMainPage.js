14128dfd41204b2da2cb4186f84d45c0fd886912
Stas <stas.mylashenko@gmail.com>
2025-08-26 17:49:49 +0300

    updated manager/BankManagerMainPage.js

--- src/pages/manager/BankManagerMainPage.js
+++ src/pages/manager/BankManagerMainPage.js
@@ -3,9 +3,37 @@ import { expect } from '@playwright/test';
 export class BankManagerMainPage {
   constructor(page) {
     this.page = page;
+    this.addCustomerButton = page.getByRole('button', { name: 'Add Customer'});
+    this.openAccountButton = page.getByRole('button', { name: 'Open Account'});
+    this.customersButton = page.getByRole('button', { name: 'Customers'});
   }
 
   async open() {
     await this.page.goto('/angularJs-protractor/BankingProject/#/manager');
   }
+
+  async clickCustomersButton() {
+    await this.customersButton.click();
+  }
+
+  async clickAddCustomerButton() {
+    await this.addCustomerButton.click();
+  }
+
+  async clickOpenAccountButton() {
+    await this.openAccountButton.click();
+  }
+
+  async visibleAddCustomerButton() {
+    await expect(this.addCustomerButton).toBeVisible();
+  }
+
+  async visibleOpenAccountButton() {
+    await expect(this.openAccountButton).toBeVisible();
+  }
+
+  async visibleCustomersButton() {
+    await expect(this.customersButton).toBeVisible();
+  }
+
 }
