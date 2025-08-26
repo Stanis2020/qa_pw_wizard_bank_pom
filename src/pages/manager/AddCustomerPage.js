35fd3834ce95983a1cd62c561e7ae24737dce002
Stas <stas.mylashenko@gmail.com>
2025-08-26 17:48:29 +0300

    updated manager/AddCustomerPage.js

--- src/pages/manager/AddCustomerPage.js
+++ src/pages/manager/AddCustomerPage.js
@@ -3,6 +3,10 @@ import { expect } from '@playwright/test';
 export class AddCustomerPage {
   constructor(page) {
     this.page = page;
+    this.firstNameField = page.getByPlaceholder('First Name');
+    this.lastNameField = page.getByPlaceholder('Last Name');
+    this.postCodeNameField = page.getByPlaceholder('Post Code');
+    this.submitFormButton = page.getByRole('form').getByRole('button', { name: 'Add Customer'});
   }
 
   async open() {
@@ -10,4 +14,21 @@ export class AddCustomerPage {
       '/angularJs-protractor/BankingProject/#/manager/addCust',
     );
   }
+
+  async fillFirstName(name) {
+    await this.firstNameField.fill(name);
+  }
+
+  async fillLastName(lastName) {
+    await this.lastNameField.fill(lastName);
+  }
+
+  async fillPostalCodeName(postCode) {
+    await this.postCodeNameField.fill(postCode);
+  }
+
+  async clickAddCustomerButton() {
+    await this.submitFormButton.click();
+  }
+
 }
