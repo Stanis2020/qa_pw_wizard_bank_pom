e220ac44ca83260dcf33659338e8e1593a11c20e
Stas <stas.mylashenko@gmail.com>
2025-08-26 17:51:39 +0300

    updated manager/OpenAccountPage.js

--- src/pages/manager/OpenAccountPage.js
+++ src/pages/manager/OpenAccountPage.js
@@ -1,13 +1,32 @@
 import { expect } from '@playwright/test';
 
-export class AddCustomerPage {
+export class OpenAccountPage {
   constructor(page) {
     this.page = page;
+    this.accountCurrencyDropDown = page.getByTestId('currency');
+    this.accountCustomersDropDown = page.getByTestId('userSelect');
+    this.processButton = page.getByRole('button', { name: 'Process'});
   }
 
   async open() {
     await this.page.goto(
       '/angularJs-protractor/BankingProject/#/manager/openAccount',
     );
+  }
+
+  async chooseDollarCurrency(currency) {
+    await this.accountCurrencyDropDown.selectOption(currency);
+  }
+
+  async chooseCustomer(customer) {
+    await this.accountCustomersDropDown.selectOption(customer);
+  }
+
+  async assertShownCurrencyValue(value) {
+    await expect(this.accountCurrencyDropDown).toHaveValue(value);
+  }
+
+  async clickProcessButton() {
+    await this.processButton.click();
   }
 }
